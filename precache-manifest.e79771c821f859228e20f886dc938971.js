67f9de5a25a873b8c1b38be2ac59f8599f3c0cd5
anajrj <anantjaiswal28031997@gmail.com>
2020-07-17 16:30:58 +0530

    Updates

---
+++ precache-manifest.e79771c821f859228e20f886dc938971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1352e32053bd29c7ed4d5f1c87f8f42a",
+    "url": "/react-ecommerce/index.html"
+  },
+  {
+    "revision": "04c279a0b9d2264b7c52",
+    "url": "/react-ecommerce/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c72af11e39a386b6148e",
+    "url": "/react-ecommerce/static/js/2.72031177.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ecommerce/static/js/2.72031177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c279a0b9d2264b7c52",
+    "url": "/react-ecommerce/static/js/main.a22412ed.chunk.js"
+  },
+  {
+    "revision": "3d5e030fb29c5737fa4f",
+    "url": "/react-ecommerce/static/js/runtime-main.05908f74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
